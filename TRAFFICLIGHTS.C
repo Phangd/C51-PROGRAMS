7424cc9688254626a0d4ffbdf916142b5b7d2d6a
ANT <53750988+Phangd@users.noreply.github.com>
2019-08-30 11:05:52 +0800

    Update TRAFFICLIGHTS.C

--- TRAFFICLIGHTS.C
+++ TRAFFICLIGHTS.C
@@ -12,9 +12,9 @@ sbit nsduan	  = P3^0;
 sbit nswei    = P3^1;
 
 code unsigned char num[]={0xc0,0xf9,0xA4,0xB0,0x99,0x92,0x82,0xf8,0x80,0x90};
-code unsigned char seg[]={0xfa,0xf5,0xf4,0xf8};
+code unsigned char seg[]={0xaa,0x55,0x44,0x88};
 uchar flag;
-uchar seconds   = 25;
+uchar seconds   = 30;
 
 void delay(uchar x){
 	uchar i,j;
@@ -40,98 +40,168 @@ void timer0_serive() interrupt 1 using 0{
 	if(flag == 200){
 		seconds--;
 		flag = 0;
-		if(seconds <= 0) seconds=25;
+		if(seconds == 0) seconds = 30;		//uchar类型没有负数,0-1后为255
 	}
 }
 
 void timeshow(){
-	uchar redshi,redge,yelloshi,yelloge,greenshi,greenge;
-	redshi 	 = (seconds-15)/10;
-	redge  	 = (seconds-15)%10;
-	yelloshi = (seconds-11)/10;
-	yelloge  = (seconds-11)%10;
-	greenshi = seconds/10;
-	greenge  = seconds%10;
-	if(seconds <= 25 && seconds >14){
+	uchar redshi,redge,yelloshi,yelloge,greenshi,greenge,yelloshi1,yelloge1;
+	redshi 	  = (seconds-20)/10;
+	redge  	  = (seconds-20)%10;
+	yelloshi  = (seconds-16)/10;
+	yelloge   = (seconds-16)%10;
+	greenshi  = (seconds-5)/10;
+	greenge   = (seconds-5)%10;
+	yelloshi1 = (seconds-1)/10;
+	yelloge1  = (seconds-1)%10;
+	
+	if(seconds <= 30 && seconds >19){
 		P2    = seg[1];
 		nswei = 1;
 		nswei = 0;
-		P2    = 0xf0;
+		P2    = 0x00;
 		
-		P2    = num[redshi];
+		P2    = num[redshi];		//南北红灯
 		nsduan= 1;
 		nsduan= 0;
-		P2    = 0xf0;
+		P2    = 0x00;
 		delay(5);
 		
 		P2    = seg[0];
 		nswei = 1;
 		nswei = 0;
-		P2    = 0xf0;
+		P2    = 0x00;
 		
 		P2    = num[redge];
 		nsduan= 1;
 		nsduan= 0;
-		P2    = 0xf0;
+		P2    = 0x00;
 		delay(5);
 	}
 	
-	if(seconds <= 14 && seconds >10){
+	if(seconds <= 19 && seconds >15){
 		P2    = seg[1];
 		nswei = 1;
 		nswei = 0;
-		P2    = 0xf0;
+		P2    = 0x00;
 		
-		P2    = num[yelloshi];
+		P2    = num[yelloshi];		//全黄
 		nsduan= 1;
 		nsduan= 0;
-		P2    = 0xf0;
+		P2    = 0x00;
 		delay(5);
 		
 		P2    = seg[0];
 		nswei = 1;
 		nswei = 0;
-		P2    = 0xf0;
+		P2    = 0x00;
 		
 		P2    = num[yelloge];
 		nsduan= 1;
 		nsduan= 0;
-		P2    = 0xf0;
+		P2    = 0x00;
 		delay(5);
 	}
 	
-	if(seconds <= 10 && seconds >=0){
+	if(seconds <= 15 && seconds >=5){
 		P2    = seg[1];
 		nswei = 1;
 		nswei = 0;
-		P2    = 0xf0;
+		P2    = 0x00;
 		
-		P2    = num[greenshi];
+		P2    = num[greenshi];		//南北绿灯
 		nsduan= 1;
 		nsduan= 0;
-		P2    = 0xf0;
+		P2    = 0x00;
 		delay(5);
 		
 		P2    = seg[0];
 		nswei = 1;
 		nswei = 0;
-		P2    = 0xf0;
+		P2    = 0x00;
 		
 		P2    = num[greenge];
 		nsduan= 1;
 		nsduan= 0;
-		P2    = 0xf0;
+		P2    = 0x00;
+		delay(5);
+	}
+	
+	if(seconds < 5 && seconds >=0){
+		P2    = seg[1];
+		nswei = 1;
+		nswei = 0;
+		P2    = 0x00;
+		
+		P2    = num[yelloshi1];		//全黄
+		nsduan= 1;
+		nsduan= 0;
+		P2    = 0x00;
+		delay(5);
+		
+		P2    = seg[0];
+		nswei = 1;
+		nswei = 0;
+		P2    = 0x00;
+		
+		P2    = num[yelloge1];
+		nsduan= 1;
+		nsduan= 0;
+		P2    = 0x00;
 		delay(5);
 	}
 }
 
 void ledshow(){
-	// if(redtime > 0) redns = 0; 
+	if(seconds <= 30 && seconds > 23){
+		P0 	  = 0xDE;				//绿红灯常亮
+	}
+	switch(seconds){				//最后4秒交通灯闪烁
+		case 23:
+			P0  = 0xFF;
+			break;
+		case 22:
+			P0	= 0xDE;
+			break;
+		case 21:
+			P0	= 0xFF;
+			break;
+		case 20:
+			P0	= 0xDE;
+			break;
+	}
+	
+	if(seconds <= 19 && seconds > 15){
+		P0 	  = 0xED;				//黄灯常亮
+	}
+	
+	if(seconds <= 15 && seconds > 8){
+		P0 	  = 0xF3;
+	}
+	switch(seconds){				//最后4秒交通灯闪烁
+		case 8:
+			P0	= 0xFF;
+			break;
+		case 7:
+			P0	= 0xF3;
+			break;
+		case 6:
+			P0	= 0xFF;
+			break;
+		case 5:
+			P0	= 0xF3;
+			break;
+	}
+	
+	if(seconds < 5 && seconds >= 0){
+		P0 	  = 0xED;				//黄灯常亮
+	}
 }
 
 void main(){
 	init_timer();
 	while(1){
 		timeshow();
+		ledshow();
 	}
 }
