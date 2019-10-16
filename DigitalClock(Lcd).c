62bf149c047b520566e3f09448186c532e9dd9f8
ANT <53750988+Phangd@users.noreply.github.com>
2019-10-16 15:26:30 +0800

    Update DigitalClock(Lcd).c

--- DigitalClock(Lcd).c
+++ DigitalClock(Lcd).c
@@ -1,4 +1,5 @@
 #include <reg52.h>
+#include <intrins.h>
 typedef unsigned char uint8;
 typedef unsigned int uint16;
 
@@ -17,6 +18,8 @@ sbit key2  = P3^3;
 sbit key3  = P3^4;
 sbit key4  = P3^5;
 
+sbit dq    = P3^7;
+
 void lcd_init();
 void wcom();
 void wdat();
@@ -27,19 +30,30 @@ void weekshow();
 void keyscan();
 void day_jud();
 void key_disposal();
+void ds18b20_init();
+uint8 byte_read();
+void byte_write();
+void temp_read();
+void temp_display(uint8 position,uint16 tvalue1);
 
-uint8 time_1s,time_250ms;
+uint8 time_1s,time_400ms;
 uint8 second,minute = 59,hour = 23;
 uint8 day = 1,month = 1,week = 1;
-uint16 year = 2019;
+uint16 year = 2019,tvalue;
 uint8 cursor;
 uint8 key_value;
-bit f_twinkle,f_mode,flag,f_pressing;
+bit f_twinkle,f_mode,f_pressing;
+
+uint8 code table1[]={0x10,0x06,0x09,0x08,0x08,0x09,0x06,0x00};			//字符℃
 
 void delay(uint16 k){
 	uint16 i,j;
 	for(i=k;i>0;i--)
 		for(j=110;j>0;j--);
+}
+
+void delayus(uint8 time_1us){
+	while(time_1us--);
 }
 
 void timer_init(){
@@ -63,8 +77,8 @@ void isr_timer0 () interrupt 1 using 0 {
 	TH0 = 0x4C;		//50ms
 	TL0 = 0x00;
 	keyscan();
-	if(++time_250ms >= 8){
-		time_250ms = 0;
+	if(++time_400ms >= 8){
+		time_400ms = 0;
 		f_twinkle = ~f_twinkle;
 	}
 }
@@ -78,7 +92,18 @@ void isr_timer1 () interrupt 3 using 1 {
 	}
 }
 
-void wcom(uint8 com){
+void ds18b20_init(){						//ds18b20初始化
+	dq = 1;	
+	_nop_();
+	dq = 0;
+	delayus(75);
+	dq = 1;
+	delayus(20);
+	dq = 1;
+	_nop_();
+}
+
+void wcom(uint8 com){						//LCD1602写命令
 	lcdrw = 0;
 	lcdrs = 0;
 	P0 = com;
@@ -87,7 +112,7 @@ void wcom(uint8 com){
 	lcden = 0;
 }
 
-void wdat(uint8 dat){
+void wdat(uint8 dat){						//LCD1602写数据
 	lcdrw = 0;
 	lcdrs = 1;
 	P0 = dat;
@@ -96,45 +121,54 @@ void wdat(uint8 dat){
 	lcden = 0;	
 }
 
-void lcd_init(){
+void lcd_init(){					//LCD初始化
 	wcom(0x38);
 	wcom(0x01);
 	wcom(0x06);
 	wcom(0x0c);
+	
+	wcom(0x86);
+	wdat(0x2d);					//"-"
+	wcom(0x89);
+	wdat(0x2d);					//"-"
+	wcom(0x80+0x40+0x04);
+	wdat(0x3a);					//":"
+	wcom(0x80+0x40+0x07);
+	wdat(0x3a);					//":"
 }
 
-void timeshow(uint8 position,uint8 time){
+void timeshow(uint8 position,uint8 time){		 //时间显示,position为显示位置
 	uint8 i,j;
-	i = time/10;				//shiwei
-	j = time%10;				//gewei
+	i = time/10;					//shiwei
+	j = time%10;					//gewei
 	wcom(0x80+0x40+position);
-	wdat(0x30+i);				//shiwei
-	wdat(0x30+j);				//gewei
+	wdat(0x30+i);					//shiwei
+	wdat(0x30+j);					//gewei
 }
 
-void dateshow(uint8 position,uint8 date1){
+void dateshow(uint8 position,uint8 date1){		//日期显示(月、日)
 	uint8 i,j;
-	i = date1/10;				//shiwei
-	j = date1%10;				//gewei
+	i = date1/10;					//shiwei
+	j = date1%10;					//gewei
 	wcom(0x80+position);
-	wdat(0x30+i);				//shiwei
-	wdat(0x30+j);				//gewei
+	wdat(0x30+i);					//shiwei
+	wdat(0x30+j);					//gewei
 }
 
-void yearshow(uint8 position,uint16 year1){
+void yearshow(uint8 position,uint16 year1){		//年份显示
 	uint8 i,j,m,n;
-	i = year1/1000;				//qianwei
-	j = year1/100%10;			//baiwei
+	i = year1/1000;					//qianwei
+	j = year1/100%10;				//baiwei
 	m = year1/10%10;
 	n = year1%10;
 	wcom(0x80+position);
-	wdat(0x30+i);				//qianwei
-	wdat(0x30+j);				//baiwei
-	wdat(0x30+m);				//shiwei
-	wdat(0x30+n);				//gewei
+	wdat(0x30+i);					//qianwei
+	wdat(0x30+j);					//baiwei
+	wdat(0x30+m);					//shiwei
+	wdat(0x30+n);					//gewei
 }
 
-void weekshow(uint8 position,uint8 week1){
+void weekshow(uint8 position,uint8 week1){		//星期显示
 	wcom(0x80+position);
 	switch(week1){
 		case 1:wdat(0x4d);wdat(0x4f);wdat(0x4e);break;
@@ -148,7 +182,7 @@ void weekshow(uint8 position,uint8 week1){
 	}
 }
 
-void day_jud(){
+void day_jud(){									//日期处理(秒、分、时、日、月进位)
 	if(second > 59){
 		second = 0;
 		if(++minute > 59){
@@ -198,43 +232,27 @@ void day_jud(){
 	}
 }
 
-void display(){
-	if(!f_mode){
-		wcom(0x86);
-		wdat(0x2d);							//"-"
-		wcom(0x89);
-		wdat(0x2d);							//"-"
-		wcom(0x80+0x40+0x06);
-		wdat(0x3a);							//":"
-		wcom(0x80+0x40+0x09);
-		wdat(0x3a);							//":"
-
+void display(){									//显示函数
+	if(!f_mode){								//时间显示模式
 		yearshow(2,year);
 		dateshow(7,month);
 		dateshow(0x0a,day);
 		weekshow(0x0d,week);
 		
-		timeshow(4,hour);
-		timeshow(7,minute);
-		timeshow(0x0a,second);
+		timeshow(2,hour);
+		timeshow(5,minute);
+		timeshow(0x08,second);
+		temp_read();							//读取温度
+		temp_display(0x0b,tvalue);					//温度显示
 	}
-	else{
-		wcom(0x86);
-		wdat(0x2d);						//"-"
-		wcom(0x89);
-		wdat(0x2d);						//"-"
-		wcom(0x80+0x40+0x06);
-		wdat(0x3a);						//":"
-		wcom(0x80+0x40+0x09);
-		wdat(0x3a);						//":"
-		wcom(0x80+0x40);
-		wdat(0x53);						//"S"
-		wdat(0x45);						//"E"
-		wdat(0x54);						//"T"
-		wdat(0x3a);						//":"
-			
-		if(f_twinkle && !f_pressing){
-			if(cursor == 0) {
+	else{									//时间调节模式
+		wcom(0x80+0x40+0x0d);
+		wdat(0x53);							//"S"
+		wdat(0x45);							//"E"
+		wdat(0x54);							//"T"
+
+		if(f_twinkle && !f_pressing){					//闪烁标志位,0.4秒取反一次,为1则相应位置显示空白
+			if(cursor == 0) {					//按压标志位,按键按下时为1,不闪烁
 				wcom(0x82);
 				wdat(0x20);
 				wdat(0x20);
@@ -258,47 +276,47 @@ void display(){
 				wdat(0x20);
 			}
 			if(cursor == 4) {
-				wcom(0x80+0x44);
+				wcom(0x80+0x42);
 				wdat(0x20);
 				wdat(0x20);
 			}
 			if(cursor == 5) {
-				wcom(0x80+0x47);
+				wcom(0x80+0x45);
 				wdat(0x20);
 				wdat(0x20);
 			}
 			if(cursor == 6) {
-				wcom(0x80+0x4a);
+				wcom(0x80+0x48);
 				wdat(0x20);
 				wdat(0x20);
 			}
 		}
-		else {
+		else {									//正常显示
 			yearshow(2,year);
 			dateshow(7,month);
 			dateshow(0x0a,day);
 			weekshow(0x0d,week);
-			timeshow(4,hour);
-			timeshow(7,minute);
-			timeshow(0x0a,second);
+			timeshow(2,hour);
+			timeshow(5,minute);
+			timeshow(0x08,second);
 		}
 	}
 }
 
-void keyscan(){
+void keyscan(){										//按键扫描
 	static uint8 key_state;
 	
 	switch(key_state){
-		case key_state0:
+		case key_state0:							//未按下
 			if(!key1 || !key2 || !key3 || !key4){
 				key_state = key_state1;
 			}
 		break;
-		case key_state1:
-			f_pressing = 1;                     //按下后置一，处理函数中不处理按键
+		case key_state1:							//有键按下
+			f_pressing = 1;
 			if(!key1){
-				TR1 = ~TR1;
-				f_mode = ~f_mode;
+				TR1 = ~TR1;						//走时停止
+				f_mode = ~f_mode;					//模式切换
 				key_value = 1;
 				key_state = key_state2;
 			}
@@ -318,39 +336,36 @@ void keyscan(){
 				key_state = key_state0;
 			}
 		break;
-		case key_state2:
+		case key_state2:							//松开按键
 			if(key1 && key2 && key3 && key4){
-				f_pressing = 0;                //松手后清零，处理按键反馈
+				f_pressing = 0;
 				key_state = key_state0;
 			}
 		break;
 	}
 }
 
-void key_disposal(){
-	if(!f_pressing){
+void key_disposal(){									//键值处理
+	if(!f_pressing){								//松开按键才处理
 		if(key_value == 1){
 			key_value = 0;
-			cursor = 0;
-			lcd_init();
+			cursor = 0;							//光标位置清零
+			lcd_init();							//清屏
 		}
 		else if(key_value == 2){
 			key_value = 0;
-			cursor++;
+			cursor++;							//光标+
 			if(cursor > 6){
 				cursor = 0;
 			}
 		}
-		else if((key_value == 3) && f_mode){
+		else if((key_value == 3) && f_mode){					//光标所处位置值+
 			key_value = 0;
 			if(cursor == 0){
 				year++;
 			}
 			else if (cursor == 1){
-				month++;
-				if(month > 12){
-					month = 1;
-				}
+				if(++month > 12) month = 1;
 			}
 			else if(cursor == 2){
 				if(++day > 31) day = 1;
@@ -368,7 +383,7 @@ void key_disposal(){
 				if(++second > 59) second = 0;
 			}
 		}
-		else if((key_value == 4) && f_mode){
+		else if((key_value == 4) && f_mode){					//光标所处位置值-
 			key_value = 0;
 			if(cursor == 0 && year > 0){
 				year--;
@@ -395,7 +410,72 @@ void key_disposal(){
 	}
 }
 
+uint8 byte_read(){									//ds18b20 读字节
+	uint8 i,j,dat;
+	for(i = 0; i < 8 ; i++){
+		dq = 0;
+		_nop_();
+		dq = 1;
+		_nop_();
+		j = dq;
+		delayus(10);
+		dq = 1;
+		_nop_();
+		dat = (j << 7 | dat >> 1);
+	}
+	return(dat);
+}
+
+void byte_write(uint8 data1){								//ds18b20 写字节
+	uint8 i;
+	for(i = 0; i < 8 ; i++){
+		dq = 0;
+		_nop_();
+		dq = data1&0x01;
+		delayus(10);
+		dq = 1;
+		_nop_();
+		data1 >>= 1;
+	}
+}
+
+void temp_read(){									//读温度
+	float data1;
+	uint8 i,j;
+	ds18b20_init();
+	byte_write(0xcc);								//向DS18B20发跳过读ROM命令
+	byte_write(0x44);								//启动DS18B20进行温度转换命令，转换结果存入内部RAM
+	delayus(80);
+	ds18b20_init();
+	byte_write(0xcc);								//向DS18B20发跳过读ROM命令
+	byte_write(0xbe);								//读取温度寄存器等（共可读9个寄存器） 前两个就是温度
+	delayus(80);
+	i = byte_read();								//内部RAM 低位
+	j = byte_read();								//内部RAM 高位
+	data1 = (j*256 + i) * 6.25;							//取出温度值
+	tvalue = (uint16)data1;								//强制转换
+}
+
+void temp_display(uint8 position,uint16 tvalue1){					//温度显示
+	uint8 i,j,m;
+	i = tvalue1/1000;
+	j = tvalue1%1000/100;
+	m = tvalue1%100/10;								//取出十位 个位 十分位
+	wcom(0x80 + 0x40 + position);
+	wdat(0x30 + i);
+	wdat(0x30 + j);
+	wdat(0x2e);									//"."
+	wdat(0x30 + m);
+	wdat(0x00);									//自编字符
+}
+
 void main(){
+	uint8 i;
+	wcom(0x40);
+	for(i = 0; i < 8 ; i++){
+		wdat(table1[i]);							//写入自编字符
+	}
+	
 	timer_init();
 	lcd_init();
 	while(1){
